29a5751bf7ddb27163a9cb73f1f4cb00ced644b8
Carissa Allen <carissawood@outlook.com>
2019-11-21 13:24:30 -0800

    Add basic project structure
    This is intended to establish project folder organization. We do not
    have to use Main.java or MainTest.java if they are not necessary for the
    project. (In other words, we may want to run our application from
    Server.java.)

---
+++ src/main/java/com/psu/cs/irc/Main.java
@@ -0,0 +1,7 @@
+package com.psu.cs.irc;
+
+public class Main {
+  public static void main(String[] args) {
+    return;
+  }
+}
---
+++ src/test/java/com/psu/cs/irc/MainTest.java
@@ -0,0 +1,4 @@
+package com.psu.cs.irc;
+
+public class MainTest {
+}
